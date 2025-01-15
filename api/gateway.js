589737f32d761782f0df8506e5f21db5fe0444a1
abhitestuserr <abhibarkade.companydevice@gmail.com>
2025-01-15 17:34:52 +0530

    Update gateway.js

--- api/gateway.js
+++ api/gateway.js
@@ -1,1 +1,26 @@
+const { ApolloServer } = require("apollo-server");
+const { ApolloGateway, IntrospectAndCompose } = require("@apollo/gateway");
 
+const gateway = new ApolloGateway({
+  supergraphSdl: new IntrospectAndCompose({
+    subgraphs: [
+      { name: "Persons", url: "https://formidable-felecia-abhi-test-org-ffc6ed09.koyeb.app/graphql" }
+    ],
+    debug: true
+  }),
+});
+
+const server = new ApolloServer({
+  gateway,
+});
+
+server
+  .listen({
+    port: 4001,
+  })
+  .then(({ url }) => {
+    console.log(`🚀 Gateway ready at ${url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
